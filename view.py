d6267a12b02fc93cdf1389b3564c9f86f59a0aec
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-12 18:09:43 +0500

    Add my_calc

--- view.py
+++ view.py
@@ -3,14 +3,28 @@ def view_data(data):
     print(data)
 
 def get_value():
-    value = input('value = ')
-    if value.find('/') != -1:
-        return Fraction(value)
-    elif value.find('j') != -1:
-        return complex(value)
-    else:
-        return int(value)
+    while True:
+        try:
+            value = input('value = ')
+            if value.find('/') != -1:
+                return Fraction(value)
+            elif value.find('j') != -1:
+                return complex(value)
+            else:
+                return int(value)
+        except ValueError:
+            print('Check entered value!')
 
 def get_math_action():
-    return input('math_action ')
+    while True:
+        math_action = input('math_action ')
+        if math_action == '+':
+            return math_action
+        elif math_action == '-':
+            return math_action
+        elif math_action == '*':
+            return math_action
+        elif math_action == '/':
+            return math_action
+        print('Check entered value!')
 
