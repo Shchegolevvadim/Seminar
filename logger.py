7b465ad7baa8682777dd2faada61e5171d16c4ac
AbubakirovRA <AbubakirovRA@ya.ru>
2022-02-13 20:59:13 +0500

    Add change logger time

--- logger.py
+++ logger.py
@@ -1,25 +1,22 @@
 from datetime import datetime as dt
 
 def logger_a(value):
-    time = dt.now().strftime('%H:%M')
+    time = dt.now().strftime('%H:%M:%S')
     with open('log.csv', 'a') as file:
         file.write('{}; value_a = {};'
                     .format(time, value))
 
 def logger_b(value):
-    time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
         file.write(' value_b = {};'
                     .format(value))
 
 def logger_math_action(value):
-    time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
         file.write('  math_action "{}";'
                     .format(value))
 
 def logger_result(value):
-    time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
         file.write(' result = {};\n'
                     .format(value))
